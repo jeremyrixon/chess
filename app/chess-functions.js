7c459cfb9d94b8a65ac1349eb308846437c86e82
jeremyrixon <jeremy@rixon.org>
2017-12-24 12:35:33 +1100

    starting

---
+++ app/chess-functions.js
@@ -0,0 +1,4 @@
+
+export function dump-board(board) {
+  console.log(board);
+}
---
+++ test/es6.js
@@ -0,0 +1,6 @@
+var assert = require('chai').assert;
+describe('es6', function() {
+  it('should return true', function() {
+    assert('function' === typeof Map);
+  });
+});
